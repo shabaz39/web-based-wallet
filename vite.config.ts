8e0eff8c8dda25da71411347c737d39109d119d0
FIRST_NAME LAST_NAME <shabazbazigar@gmail.com>
2024-10-15 09:41:22 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/web-based-wallet/'
+  base:"/web-based-wallet/",
 })
